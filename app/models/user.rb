e27b16d7127999298304c865e2675ba172ba96ac
i29ri <g1g1yuto@gmail.com>
2020-12-04 04:47:56 +0000

    destroy add

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定します。
+  has_many :relationship, class_name: "relationships", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "relationships", foreign_key: "follower_id", dependent: :destroy
+  # @user.booksのように、@user.yyyで、
+  # そのユーザがフォローしている人orフォローされている人の一覧を出したい
+  has_many :yyy, through: :xxx, source: :zzz
+  
   attachment :profile_image
 end
